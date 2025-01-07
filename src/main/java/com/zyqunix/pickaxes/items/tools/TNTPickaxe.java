377e59b6de69d98ae859b01fbd75c4c6b23b7822
zyqunix <fabiovanek2008@gmail.com>
2025-01-07 21:07:25 +0100

    removed tnt/fireball spawns. made it an explosion instead

--- src/main/java/com/zyqunix/pickaxes/items/tools/TNTPickaxe.java
+++ src/main/java/com/zyqunix/pickaxes/items/tools/TNTPickaxe.java
@@ -1,7 +1,6 @@
 package com.zyqunix.pickaxes.items.tools;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityLargeFireball;
 import net.minecraft.item.ItemStack;
@@ -10,7 +9,6 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.ProjectileImpactEvent;
 
 public class TNTPickaxe extends ToolPickaxe {
 
@@ -22,6 +20,11 @@ public class TNTPickaxe extends ToolPickaxe {
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
         ItemStack item = player.getHeldItem(hand);
+
+        float explosionStrength = 5.0f;
+        world.createExplosion(null, player.posX, player.posY, player.posZ, explosionStrength, true);
+
+        /*
         Vec3d aim = player.getLookVec();
 
         EntityLargeFireball fireball = new EntityLargeFireball(world, player, 1, 1, 1);
@@ -30,10 +33,10 @@ public class TNTPickaxe extends ToolPickaxe {
         fireball.accelerationY = aim.y * 0.1;
         fireball.accelerationZ = aim.z * 0.1;
         fireball.explosionPower = 5;
+        fireball.extinguish();
 
         world.spawnEntity(fireball);
 
-        /*
         EntityTNTPrimed tnt = new EntityTNTPrimed(world, 1, 1, 1, player);
         tnt.setPosition(player.posX + aim.x * 1.5D, player.posY + aim.y * 1.5D, player.posZ + aim.z * 1.5D);
         tnt.setFuse(80);
