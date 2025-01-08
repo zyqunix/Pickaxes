8ca395a7e8664806ee657370b8c4644d2f9b2a00
zyqunix <fabiovanek2008@gmail.com>
2025-01-08 22:51:38 +0100

    ultimate stuff

--- src/main/java/com/zyqunix/pickaxes/init/ModItems.java
+++ src/main/java/com/zyqunix/pickaxes/init/ModItems.java
@@ -26,7 +26,8 @@ public class ModItems {
     public static final Item.ToolMaterial MATERIAL_RUBY = EnumHelper.addToolMaterial("material_ruby", 3, 830, 5.0f, 4.0f, 15);
     public static final Item.ToolMaterial MATERIAL_COBALT = EnumHelper.addToolMaterial("material_cobalt", 3, 960, 6.0f, 4.0f, 10);
     public static final Item.ToolMaterial MATERIAL_TNT = EnumHelper.addToolMaterial("material_tnt", 3, 100, 5.0f, 4.0f, 10);
-    public static final ItemArmor.ArmorMaterial ARMOR_MATERIAL_MINING = EnumHelper.addArmorMaterial("armor_material_mining", Reference.MOD_ID + ":mining_armor", 14, new int[]{1, 2, 3, 1}, 10, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0f);
+    public static final Item.ToolMaterial MATERIAL_ULTIMATE = EnumHelper.addToolMaterial("material_ultimate", 5, 10000, 10.0f, 10.0f, 20);
+    public static final ItemArmor.ArmorMaterial ARMOR_MATERIAL_MINING = EnumHelper.addArmorMaterial("armor_material_mining", Reference.MOD_ID + ":mining_armor", 14, new int[]{2, 3, 4, 2}, 10, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0f);
 
     // Items
     public static final Item CRYSTALLIZED_RUBY = new ItemBase("crystallized_ruby");
@@ -37,12 +38,14 @@ public class ModItems {
     public static final Item ULTIMATE_PIECE_LEFT = new ItemBase("ultimate_piece_left");
     public static final Item ULTIMATE_PIECE_MIDDLE = new ItemBase("ultimate_piece_middle");
     public static final Item ULTIMATE_PIECE_RIGHT = new ItemBase("ultimate_piece_right");
+    public static final Item ULTIMATE_HANDLE = new ItemBase("ultimate_handle");
+    public static final Item ULTIMATE_STICK = new ItemBase("ultimate_stick");
 
     // Tools
     public static final ItemPickaxe RUBY_PICKAXE = new ToolPickaxe("ruby_pickaxe", MATERIAL_RUBY);
     public static final ItemPickaxe COBALT_PICKAXE = new ToolPickaxe("cobalt_pickaxe", MATERIAL_COBALT);
     public static final ItemPickaxe TNT_PICKAXE = new TNTPickaxe("tnt_pickaxe", MATERIAL_TNT);
-    public static final ItemPickaxe ULTIMATE_PICKAXE = new UltimatePickaxe("ultimate_pickaxe", MATERIAL_TNT);
+    public static final ItemPickaxe ULTIMATE_PICKAXE = new UltimatePickaxe("ultimate_pickaxe", MATERIAL_ULTIMATE);
 
     // Armor
     public static final Item MINING_HELMET = new MiningHelmet(ARMOR_MATERIAL_MINING, 1, EntityEquipmentSlot.HEAD);
