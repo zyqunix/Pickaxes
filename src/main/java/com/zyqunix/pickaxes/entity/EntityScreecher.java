d875f248eda04a996cd928629cefd08fd1357fc5
zyqunix <fabiovanek2008@gmail.com>
2025-01-29 22:44:09 +0100

    some changes

--- src/main/java/com/zyqunix/pickaxes/entity/EntityScreecher.java
+++ src/main/java/com/zyqunix/pickaxes/entity/EntityScreecher.java
@@ -11,14 +11,18 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class EntityScreecher extends EntityCaveSpider {
     private boolean tamed = false;
+    private boolean sitting = false;
     private EntityLivingBase owner;
 
     public EntityScreecher(World worldIn) {
         super(worldIn);
         this.setSize(1F, 0.7F);
+        this.setHealth(4.0F);
     }
 
     @Override
@@ -30,32 +34,39 @@ public class EntityScreecher extends EntityCaveSpider {
     @Override
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(40.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3D);
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(4.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.2D);
         this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(4.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_SPEED).setBaseValue(1.0D);
     }
 
     @Override
     public void onUpdate() {
         super.onUpdate();
-        if (!tamed && this.getAttackTarget() instanceof EntityPlayer) {
-            EntityPlayer player = (EntityPlayer) this.getAttackTarget();
-            this.setPosition(player.posX, player.posY + player.getEyeHeight(), player.posZ);
-            if (!player.isSneaking() && !player.isSpectator()) {
-                player.attackEntityFrom(DamageSource.causeMobDamage(this), 1.0F);
-            }
-        } else if (tamed && owner != null) {
-            this.getNavigator().tryMoveToEntityLiving(owner, 1.0D);
-            if (this.getAttackTarget() == null) {
-                this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityLiving.class, 10, true, false, entity -> entity.isCreatureType(EnumCreatureType.MONSTER, false)));
+        this.fallDistance = 0.0F;
+
+        if (!sitting) {
+            if (!tamed && this.getAttackTarget() instanceof EntityPlayer) {
+                EntityPlayer player = (EntityPlayer) this.getAttackTarget();
+                this.setPosition(player.posX, player.posY + player.getEyeHeight(), player.posZ);
+                if (!player.isSneaking() && !player.isSpectator()) {
+                    if (!player.isEntityInvulnerable(DamageSource.GENERIC)) {
+                        player.attackEntityFrom(DamageSource.causeMobDamage(this), 1.0F);
+                    }
+                }
+            } else if (tamed && owner != null) {
+                this.getNavigator().tryMoveToEntityLiving(owner, 1.0D);
+                if (this.getAttackTarget() == null) {
+                    this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityLiving.class, 10, true, false, entity -> entity.isCreatureType(EnumCreatureType.MONSTER, false)));
+                }
             }
         }
     }
 
     @Override
     public boolean attackEntityFrom(DamageSource source, float amount) {
-        if (source == DamageSource.CACTUS || source == DamageSource.IN_WALL || source.isExplosion()) {
-            return false; // Prevents dying from environmental damage.
+        if (source.getTrueSource() == this || source == DamageSource.CACTUS || source == DamageSource.IN_WALL || source.isExplosion() || source == DamageSource.FALL) {
+            return false;
         }
         return super.attackEntityFrom(source, amount);
     }
@@ -63,6 +74,7 @@ public class EntityScreecher extends EntityCaveSpider {
     @Override
     public boolean processInteract(EntityPlayer player, EnumHand hand) {
         ItemStack itemstack = player.getHeldItem(hand);
+
         if (!tamed && itemstack.getItem() == ModItems.CRYSTALLIZED_RUBY) {
             if (!player.capabilities.isCreativeMode) {
                 itemstack.shrink(1);
@@ -70,8 +82,23 @@ public class EntityScreecher extends EntityCaveSpider {
             this.tamed = true;
             this.setAttackTarget(null);
             this.owner = player;
+            this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(this.getMaxHealth() + 0.5D);
             return true;
         }
+
+        if (tamed && itemstack.getItem() == ModItems.COBALT_INGOT) {
+            if (!player.capabilities.isCreativeMode) {
+                itemstack.shrink(1);
+            }
+            this.getEntityAttribute(SharedMonsterAttributes.ATTACK_SPEED).setBaseValue(this.getEntityAttribute(SharedMonsterAttributes.ATTACK_SPEED).getBaseValue() + 0.1D);
+            return true;
+        }
+
+        if (tamed && player.isSneaking()) {
+            this.sitting = !this.sitting;
+            return true;
+        }
+
         return super.processInteract(player, hand);
     }
 
@@ -92,5 +119,9 @@ public class EntityScreecher extends EntityCaveSpider {
 
     public float getEyeHeight() {
         return 0.25F;
+    }
+
+    public static void registerSpawn() {
+        EntityRegistry.addSpawn(EntityScreecher.class, 100, 1, 3, EnumCreatureType.MONSTER, Biome.getBiome(0));
     }
 }
